fc71afc573a26a098f50c9084b90cb07e3328b53
renzocastro91 <renzogustavocastro99@gmail.com>
2023-06-05 10:59:41 -0300

    Arreglando Errores Lint

--- src/app/components/Product/index.tsx
+++ src/app/components/Product/index.tsx
@@ -1,34 +1,30 @@
-import { useState } from "react"
-import styles from "./styles.module.css"
-
+import { useState } from "react";
+import styles from "./styles.module.css";
 
 type ProductProps = {
-  name: string
-  description: string
-  price: number
-  cant: number
-  tot: number
-  onTotChange: (newTot: number) => void
+  name: string;
+  description: string;
+  price: number;
+  cant: number;
+  tot: number;
+  onTotChange: (newTot: number) => void;
 };
 
 function Product(props: ProductProps) {
-  const [cant, setCant] = useState(props.cant)
-  const [tot, setTot] = useState(props.tot)
-  const [price, setPrice] = useState(props.price)
+  const [cant, setCant] = useState(props.cant);
 
   function handleTot() {
-    const newTot = cant * price
-    setTot(newTot)
-    props.onTotChange(newTot)
+    const newTot = cant * props.price;
+    props.onTotChange(newTot);
   }
 
   function handleIncrement() {
-    setCant(cant + 1)
+    setCant(cant + 1);
   }
 
   function handleDecrement() {
     if (cant > 0) {
-      setCant(cant - 1)
+      setCant(cant - 1);
     }
   }
 
@@ -44,7 +40,7 @@ function Product(props: ProductProps) {
       </div>
       <button className={styles.buttonAdd} onClick={handleTot}>Agregar al carrito</button>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
